d11bbee99eecc9825e68f62635424df2e641dd39
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-07-15 23:20:16 +0530

    sudoku solved successfully✅

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -129,3 +129,37 @@ def check_if_white(img):
         return True
     else:
         return False
+
+
+def reshape_digits(digits): 
+
+    data = pd.read_csv(file)
+
+    feat_raw = data['quizzes']
+    label_raw = data['solutions']
+
+    feat = []
+    label = []
+
+    for i in feat_raw:
+    
+        x = np.array([int(j) for j in i]).reshape((9,9,1))
+        feat.append(x)
+    
+    feat = np.array(feat)
+    feat = feat/9
+    feat -= .5    
+    
+    for i in label_raw:
+    
+        x = np.array([int(j) for j in i]).reshape((81,1)) - 1
+        label.append(x)   
+    
+    label = np.array(label)
+    
+    del(feat_raw)
+    del(label_raw)    
+
+    x_train, x_test, y_train, y_test = train_test_split(feat, label, test_size=0.2, random_state=42)
+    
+    return x_train, x_test, y_train, y_test
---
+++ solver/solver.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from helpers.constants import MODEL_PATH
+from keras.models import load_model
+
+class Solver:
+
+    def __init__(self):
+        self.output = []
+        self.model = None
+    
+
+    def load_solver_model(self, model_name):
+        self.model = load_model(MODEL_PATH + model_name)
+
+
+    def solve(self, digits):
+        inp = np.array(digits).reshape((1, 9, 9, 1))
+        inp = inp / 9
+        inp -= .5    
+        opt = self.model.predict(inp)
+        
+        for number in opt[0]:
+            self.output.append(np.argmax(number) + 1)
+
+        print(self.output)
+        return self.output
